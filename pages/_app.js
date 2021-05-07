4ffce83be9ef55474777f5ce235799696c9915ca
huynhtan1808 <tanhuynh1808@gmail.com>
2021-05-08 00:42:26 +0700

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+import { AuthProvider } from '../lib/auth';
+import theme from '../styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+    <AuthProvider>
+      <CSSReset/>
+      <Component {...pageProps} />
+    </AuthProvider>
+    </ThemeProvider>
 }
 
-export default MyApp
+export default MyApp;
